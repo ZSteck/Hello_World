114ae215efc6663d96be8b1d1c98cb1b57d69aba
ZSteck <zsteck046@gmail.com>
2016-10-26 13:37:30 -0500

    changes 10-26-2016

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hello_World
+Hey there partner
+
+CHANGELOG
+added helloworld.py - 10/26/2016
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#helloworld.py
+#Zack Steck
+#10-26-2016
+
+''' Program that tells the world 'Hello' '''
+
+print("Hello!")
